93392897d6babc27025dc2a3b90565377b25e365
kvsnr113 <kvsnrprojkt113@gmail.com>
2022-05-09 10:52:12 +0000

    Initialize for vayu/bhima

--- anykernel.sh
+++ anykernel.sh
@@ -4,16 +4,16 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=// R.Y.N Kernel \\
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=vayu
+device.name2=bhima
+device.name3=
+device.name4=
 device.name5=
 supported.versions=
 supported.patchlevels=
