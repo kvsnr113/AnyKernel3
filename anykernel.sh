d0e5da22c8a48b8af4ef811d69547bf98a775e4e
kvsnr113 <kvsnrprojkt113@gmail.com>
2024-06-04 15:51:22 +0700

    add anykernel3 for nova kernel
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -1,27 +1,63 @@
+# AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
+## AnyKernel setup
+# begin properties
 properties() { '
-kernel.string=-R.Y.N Kernel-
+kernel.string=NOVA-KERNEL
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=vayu
-device.name2=bhima
+device.name1=munch
+device.name2=
 device.name3=
 device.name4=
 device.name5=
 supported.versions=
-supported.patchlevels=
 '; } # end properties
 
+# shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+is_slot_device=1;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 750 750 $ramdisk/*;
+
+
+## AnyKernel install
+split_boot;
+
+# Begin Ramdisk Changes
+
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
+
+flash_boot;
+## end install
+
+## vendor_boot shell variables
+block=/dev/block/bootdevice/by-name/vendor_boot;
+is_slot_device=1;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
-. tools/ak3-core.sh;
+# reset for vendor_boot patching
+reset_ak;
 
-dump_boot;
-write_boot;
+# vendor_boot install
+split_boot;
+
+flash_boot;
+## end vendor_boot install
