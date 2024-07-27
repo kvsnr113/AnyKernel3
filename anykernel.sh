9ecbcdb47127caa1a6c559114726c151679b80b5
kvsnr113 <kvsnrprojkt113@gmail.com>
2024-07-27 17:01:36 +0700

    update

--- anykernel.sh
+++ anykernel.sh
@@ -1,33 +1,49 @@
 # AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
-## AnyKernel setup
-# begin properties
-properties() { '
-kernel.string=NOVA Kernel by Project 113
-do.devicecheck=1
-do.modules=0
-do.systemless=1
-do.cleanup=1
-do.cleanuponabort=0
-device.name1=munch
-device.name2=munchin
-device.name3=alioth
-device.name4=aliothin
-device.name5=apollo
-device.name6=apollon
-supported.versions=
-'; } # end properties
+is_apollo=0;
+is_munch=0;
+is_alioth=0;
 
 block=/dev/block/bootdevice/by-name/boot;
 ramdisk_compression=auto;
 
-is_apollo=0;
-
 if [ $is_apollo == "1" ]; then
   is_slot_device=0;
-else
+  properties() { '
+  kernel.string=NOVA Kernel by Project 113
+  do.devicecheck=1
+  do.modules=0
+  do.systemless=1
+  do.cleanup=1
+  do.cleanuponabort=0
+  device.name1=apollo
+  device.name2=apollon
+  supported.versions= '; }
+elif [ $is_munch == "1" ]; then
   is_slot_device=1;
+  properties() { '
+  kernel.string=NOVA Kernel by Project 113
+  do.devicecheck=1
+  do.modules=0
+  do.systemless=1
+  do.cleanup=1
+  do.cleanuponabort=0
+  device.name1=munch
+  device.name2=munchin
+  supported.versions= '; }
+elif [ $is_alioth == "1" ]; then
+  is_slot_device=1;
+  properties() { '
+  kernel.string=NOVA Kernel by Project 113
+  do.devicecheck=1
+  do.modules=0
+  do.systemless=1
+  do.cleanup=1
+  do.cleanuponabort=0
+  device.name1=alioth
+  device.name2=aliothin
+  supported.versions= '; }
 fi;
 
 ## AnyKernel methods (DO NOT CHANGE)
